ab3fbd7525bb7483bd5de74f60dd3d00d6f0aaec
Michael Hoser <michael.hoser@outlook.com>
2024-10-12 21:32:54 +0200

    Add VSCode config, url rewrite

--- www/index.php
+++ www/index.php
@@ -4,12 +4,17 @@ require __DIR__ . "/../vendor/autoload.php";
 
 $app = new Leaf\App;
 
-// patch current working dir as "vendor" is not a sub-folder of the app
+// Required by DevTools:
+// set current working to parent as "vendor" is not a sub-folder of the app
 chdir(__DIR__ . "/..");
 \Leaf\DevTools::install("/devtools");
 
 $app->get("/", function () use ($app) {
   $app->response()->json(["message" => "Hello World!"]);
+});
+
+$app->get("/hello/{name}", function ($name) use ($app) {
+  $app->response()->json(["greeting" => "Hello " . ucfirst($name) . "!"]);
 });
 
 $app->run();
