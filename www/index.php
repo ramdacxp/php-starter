073fd831bb5bd0c059fa240809fcb68d5ead6b9a
Michael Hoser <michael.hoser@outlook.com>
2024-11-03 23:04:44 +0100

    DevTools only within PHP Dev Server

--- www/index.php
+++ www/index.php
@@ -21,10 +21,12 @@ $config->initDatabaseTables($db);
 // auth
 $auth = new App\Services\AuthService($db);
 
-// devtools
-// important: set cwd to parent as "vendor" is not a sub-folder of the app
-chdir(__DIR__ . DIRECTORY_SEPARATOR . "..");
-\Leaf\DevTools::install("/devtools");
+// devtools (only if running in PHP dev server!)
+if (strpos($_SERVER["SERVER_SOFTWARE"], "Development Server") !== false) {
+  // important: set cwd to parent as "vendor" is not a sub-folder of the app
+  chdir(__DIR__ . DIRECTORY_SEPARATOR . "..");
+  \Leaf\DevTools::install("/devtools");
+}
 
 // -----------------------------------------------------------------------------
 // ROUTES
